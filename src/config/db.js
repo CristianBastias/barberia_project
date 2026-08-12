0f90fe4e00185ab269bebf7520f38faebaae4749
CristianBastias <cristiandanielbastias@yahoo.com.ar>
2026-08-12 18:59:07 -0300

    Refactor: migrar app.js a server.js y actualizar conexion DB

--- src/config/db.js
+++ src/config/db.js
@@ -10,9 +10,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: false
 });
 
 module.exports = pool;
